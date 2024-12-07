43e2501eb60652e5af52b78a3ea3dd971e2288da
Gargoros <mikhailzhachko@gmail.com>
2024-12-07 11:52:05 +0300

    feat: init base game project

---
+++ AngryBirdsClone/AngryBirdsClone/GameViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GameViewController.swift
+//  AngryBirdsClone
+//
+//  Created by MIKHAIL ZHACHKO on 7.12.24.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
