25aafdbcf86c0d4c4088dd5c1e470adfb024ae00
Gargoros <mikhailzhachko@gmail.com>
2024-12-07 14:39:50 +0300

    feat: added bird class with base physics

---
+++ AngryBirdsClone/AngryBirdsClone/CustomNodes/Bird.swift
@@ -0,0 +1,42 @@
+//
+//  Bird.swift
+//  AngryBirdsClone
+//
+//  Created by MIKHAIL ZHACHKO on 7.12.24.
+//
+
+import SpriteKit
+
+enum BirdType: String {
+    case red, blue, yellow, gray
+}
+
+class Bird: SKSpriteNode {
+    
+    var birdType: BirdType
+    var grabbed: Bool = false
+    var flying: Bool = false {
+        didSet {
+            if flying {
+                physicsBody?.isDynamic = true
+                physicsBody?.affectedByGravity = false
+            }
+        }
+    }
+    
+    init(birdType: BirdType) {
+        self.birdType = birdType
+        let color: UIColor?
+        switch birdType {
+        case .red: color = UIColor.red
+        case .blue: color = UIColor.blue
+        case .yellow: color = UIColor.yellow
+        case .gray: color = UIColor.lightGray
+        }
+        super.init(texture: nil, color: color!, size: CGSize(width: 40.0, height: 40.0))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- AngryBirdsClone/AngryBirdsClone/Scene/GameScene.swift
+++ AngryBirdsClone/AngryBirdsClone/Scene/GameScene.swift
@@ -15,7 +15,11 @@ class GameScene: SKScene {
     let gameCamera = GameCamera()
     var panRecognizer = UIPanGestureRecognizer()
     var pinchRecognizer = UIPinchGestureRecognizer()
+    
     var maxScale: CGFloat = 0.0
+    
+    var bird = Bird(birdType: .red)
+    let anchor = SKNode()
     
     //MARK: - Views
     override func didMove(to view: SKView) {
@@ -27,6 +31,7 @@ class GameScene: SKScene {
 }
 //MARK: - CONFIG
 extension GameScene {
+    //MARK: - SETUP
     private func setupLevel(){
         if let mapNode = childNode(withName: "Tile Map Node") as? SKTileMapNode {
             self.mapNode = mapNode
@@ -34,7 +39,13 @@ extension GameScene {
         }
         
         addCamera()
+        
+        anchor.position = CGPoint(x: self.mapNode.frame.midX / 2, y: self.mapNode.frame.midY / 2)
+        addChild(anchor)
+        
+        addBird()
     }
+    //MARK: - CAMERA
     private func addCamera(){
         guard let view = view else { return }
         gameCamera.position = CGPoint(x: view.bounds.size.width / 2, y: view.bounds.size.height / 2)
@@ -42,6 +53,7 @@ extension GameScene {
         gameCamera.setConstraints(with: self, and: mapNode.frame, to: nil)
         addChild(gameCamera)
     }
+    //MARK: - GESTURE SETUP
     private func setupGestureRecognizers(){
         guard let view = view else { return }
         panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
@@ -49,15 +61,37 @@ extension GameScene {
         view.addGestureRecognizer(panRecognizer)
         view.addGestureRecognizer(pinchRecognizer)
     }
+    //MARK: - BIRD
+    private func addBird(){
+        bird.physicsBody = SKPhysicsBody(rectangleOf: bird.size)
+        bird.physicsBody?.categoryBitMask = PhysicsCategory.bird
+        bird.physicsBody?.contactTestBitMask = PhysicsCategory.all
+        bird.physicsBody?.collisionBitMask = PhysicsCategory.block | PhysicsCategory.edge
+        bird.physicsBody?.isDynamic = false
+        bird.position = anchor.position
+        addChild(bird)
+        constraintToAnchor(active: true)
+    }
+    private func constraintToAnchor(active: Bool) {
+        if active {
+            let slingRange = SKRange(lowerLimit: 0.0, upperLimit: bird.size.width * 3)
+            let positionConstraint = SKConstraint.distance(slingRange, to: anchor)
+            bird.constraints = [positionConstraint]
+        } else {
+            bird.constraints?.removeAll()
+        }
+    }
 }
 //MARK: - GESTURE
 extension GameScene {
+    //MARK: - PAN
     @objc func pan(sender: UIPanGestureRecognizer) {
         guard let view = view else { return }
         let translation = sender.translation(in: view) * gameCamera.yScale
         gameCamera.position = CGPoint(x: gameCamera.position.x - translation.x, y: gameCamera.position.y + translation.y)
         sender.setTranslation(CGPoint.zero, in: view)
     }
+    //MARK: - PINCH
     @objc func pinch(sender: UIPinchGestureRecognizer) {
         guard let view = view else { return }
         if sender.numberOfTouches == 2 {
@@ -81,6 +115,39 @@ extension GameScene {
                 sender.scale = 1.0
                 gameCamera.setConstraints(with: self, and: mapNode.frame, to: nil)
             }
+        }
+    }
+}
+//MARK: - TOUCHES
+extension GameScene {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            if bird.contains(location) {
+                panRecognizer.isEnabled = false
+                bird.grabbed = true
+                bird.position = location
+            }
+        }
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            if bird.grabbed {
+                let location = touch.location(in: self)
+                bird.position = location
+            }
+        }
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if bird.grabbed {
+            bird.grabbed = false
+            bird.flying = true
+            constraintToAnchor(active: false)
+            let dx = anchor.position.x - bird.position.y
+            let dy = anchor.position.y - bird.position.y
+            let impulse = CGVector(dx: dx, dy: dy)
+            bird.physicsBody?.applyImpulse(impulse)
+            bird.isUserInteractionEnabled = false
         }
     }
 }
